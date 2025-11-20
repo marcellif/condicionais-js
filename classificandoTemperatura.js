fbdca0f5514670564ba3ff28f49fdf9a47be71c6
marcelli <marcelli.faria@gmail.com>
2025-11-20 20:40:30 -0300

    temperatura

---
+++ classificandoTemperatura.js
@@ -0,0 +1,9 @@
+const temperatura = 26;
+
+if (temperatura < 15) {
+    console.log('frio');
+} else if (temperatura >= 15  && temperatura <= 25) {
+    console.log('agradável');
+} else {
+    console.log('quente');
+}
